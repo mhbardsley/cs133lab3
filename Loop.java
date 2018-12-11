03c86baff846b59e6805606507caa81a7069cedc
Matthew Bardsley <43888969+mhbardsley@users.noreply.github.com>
2018-12-11 17:33:01 +0000

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,7 @@
+public class Loop {
+    public static void main(String args[]) {
+        System.out.println("Do not leave running, especially on Joshua. We know who you are :)");
+        System.out.println("Going into infinite loop...");
+        while (true);
+    }
+}
